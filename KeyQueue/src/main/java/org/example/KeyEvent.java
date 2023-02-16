0686055f1d1bf37946d1224dce6b95517a578620
VPetrowDev <vladislav.petrow@gmail.com>
2023-02-16 13:31:57 +0200

    Updated solution for KeyEvent.

--- KeyQueue/src/main/java/org/example/KeyEvent.java
+++ KeyQueue/src/main/java/org/example/KeyEvent.java
@@ -4,37 +4,60 @@ import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class KeyEvent {
-    public ConcurrentLinkedQueue<Message> myQueue;
-    public Set<Message> mySet;
+    ConcurrentLinkedQueue<Message> myQueue;
+    private final Set<Message> mySet;
+    private final Queue<Message> tempQueue = new ConcurrentLinkedQueue<>();
 
     public KeyEvent(ConcurrentLinkedQueue<Message> myQueue, Set<Message> mySet) {
         this.myQueue = myQueue;
         this.mySet = mySet;
     }
 
-    public void addEvent(Message event){
-        Queue<Message> tempQueue = new LinkedList<>();
-        if(mySet.add(event)){
 
+    public void addEvent(Message event) {
+        boolean added;
+
+        synchronized (mySet) {
+            added = mySet.add(event);
+        }
+        if (added) {
             myQueue.add(event);
-            System.out.println("Added successfully to the queue!" + event.getMessage());
-
-        }else {
-            System.out.println("Oops we have the same event in the set already. Wait in the array!" + event.getMessage());
+            System.out.println("Added successfully to the queue! " + event.getMessage());
+        } else {
+            System.out.println("Oops we have the same event in the set already. Wait in the array! " + event.getMessage());
             tempQueue.add(event);
         }
 
-        if(!myQueue.contains(event)){
+        Message duplicateMessage = null;
+        synchronized (mySet) {
+            if (!myQueue.contains(event)) {
 
-            System.out.println("Cheking if this event has been processed. If yes, add it to the queue!");
+                System.out.println("Checking if this event has been processed. If yes, add it to the queue!");
+                duplicateMessage = tempQueue.poll();
+                mySet.add(duplicateMessage);
 
-            Message duplicateMessage = tempQueue.poll();
+            }
+        }
+
+        if (duplicateMessage != null) {
             myQueue.add(duplicateMessage);
             tempQueue.remove(duplicateMessage);
         }
     }
-    public Message removeEvent(Message event){
-        System.out.println("Do you consume?");
-        return myQueue.poll();
+
+    public Message removeEvent(Message event) {
+        synchronized (mySet) {
+            if (tempQueue.contains(event)) {
+                // The event is a duplicate, so don't remove it from the queue
+                System.out.println("Event is a duplicate, so not removing from the queue: " + event.getMessage());
+                return null;
+            } else {
+                // The event is not a duplicate, so remove it from the queue and the set
+                mySet.remove(event);
+                System.out.println("Removed event from the set: " + event.getMessage());
+                return myQueue.poll();
+            }
+        }
     }
+
 }
--- KeyQueue/src/main/java/org/example/Main.java
+++ KeyQueue/src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
